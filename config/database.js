4542dc0275be3de6129d36a7b788aa10cfca55e5
Alec <alec.keeler@gmail.com>
2021-02-11 18:19:14 -0600

    First commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const {
+    db: { username, password, database, host },
+} = require('./index');
+
+module.exports = {
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+        logging: false
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+    }
+};
