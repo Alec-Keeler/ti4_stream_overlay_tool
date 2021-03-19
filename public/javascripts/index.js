93d45ce42f7c9fe6f91029604c49ea942723ff46
Alec <alec.keeler@gmail.com>
2021-03-18 19:14:10 -0500

    fixed merge conflict

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -69,7 +69,6 @@ window.addEventListener('DOMContentLoaded', event => {
                     initExtract(data);
                     //deprecated from points-overview-dom
                     // initLaw(data);
-<<<<<<< HEAD
                     updatePointsBoard(data);
                     standardUpdate(data);
                     updateTechOverview(data);
@@ -77,9 +76,6 @@ window.addEventListener('DOMContentLoaded', event => {
                     updateResourceOverview(data);
                     updateLawOverview(data);
                     loop = setInterval(loopFetch, 3000);
-=======
-                    loop = setInterval(loopFetch, 20000);
->>>>>>> master
                 });
             });
     });
